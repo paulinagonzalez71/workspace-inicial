c6fa7d5c6c7c82a2446a4ed55f48d4de268aa669
paulinagonzalez71 <paulina.g.ramones@gmail.com>
2022-08-14 23:21:18 -0300

    Cambios entrega 1

---
+++ js/login.js
@@ -0,0 +1,7 @@
+let formularioLogin = document.getElementById('formulario-login');
+formularioLogin.addEventListener('submit', function(evento) {
+    evento.preventDefault();
+
+    evento.preventDefault();
+    window.location.href = 'index.html';
+})
--- js/products.js
+++ js/products.js
@@ -8,7 +8,32 @@ fetch(CARS_URL)
      return respuesta.json()
 })
 .then(function(datos) {
-    autosArray = datos.results;
+    console.log('dentro del then')
+    autosArray = datos.products;
     console.log(autosArray)
 
+    let divListaAutos = document.getElementById('lista-autos');
+
+    let htmlContentToAppend = '';
+    for (let auto of autosArray) {
+        htmlContentToAppend += `
+        <div class="col-3">
+           <img src="${auto.image}" alt="product image" class="img-thumbnail">
+        </div>
+        <div class="col">
+          <div class="d-flex w-100 justify-content-between">
+              <div class="textos">
+                  <h4>${auto.name} - ${auto.currency} ${auto.cost}</h4> 
+                  <p>${auto.description}</p> 
+              </div>
+            <small class="text-muted">${auto.soldCount} vendidos</small> 
+        </div>
+        `;
+    }
+    divListaAutos.innerHTML += htmlContentToAppend;
+
+
+
 });
+
+console.log('después de fetch y then')
